183bade436a3d36495056ca3c7ffbdcb6b7aee95
Kasper Søballe Poulsen <eaa23kapo@students.eaaa.dk>
2025-05-19 16:23:50 +0200

    Initializer, Context, DBConnection

---
+++ DAL/context/Initializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.context
+{
+    internal class Initializer : CreateDatabaseIfNotExists<WorkLoggerContext>
+    {
+        protected override void Seed(DAL.context.WorkLoggerContext context)
+        {
+            model.Afdeling afdeling1 = new model.Afdeling("IT & Udvikling", 101);
+            model.Afdeling afdeling2 = new model.Afdeling("Økonomi", 102);
+            model.Afdeling afdeling3 = new model.Afdeling("HR & Personale", 103);
+
+            context.Afdelinger.Add(afdeling1);
+            context.Afdelinger.Add(afdeling2);
+            context.Afdelinger.Add(afdeling3);
+
+            context.Medarbejdere.Add(new model.Medarbejder("KSP","200802-1234", "Kasper", afdeling1));
+            context.Medarbejdere.Add(new model.Medarbejder("MWTB", "271002-4321", "Malthe", afdeling3));
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ DAL/context/WorkLoggerContext.cs
@@ -0,0 +1,30 @@
+﻿using DAL.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace DAL.context
+{
+    public class WorkLoggerContext : DbContext
+    {
+        public WorkLoggerContext() : base("WorkLoggerContext")
+        {
+            this.Configuration.LazyLoadingEnabled = true;
+        }
+
+        public DbSet<Afdeling> Afdelinger { get; set; }
+        public DbSet<Medarbejder> Medarbejdere { get; set; }
+        public DbSet<Sag> Sager { get; set; }
+        public DbSet<Tidsregistrering> Tidsregistreringer { get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
