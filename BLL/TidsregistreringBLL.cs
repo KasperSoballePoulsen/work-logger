1c61a4131f977e9b5cf9fc8f1366cc543ea83baa
Kasper Søballe Poulsen <eaa23kapo@students.eaaa.dk>
2025-05-22 12:45:46 +0200

    Man kan opdatere medarbejder

--- BLL/TidsregistreringBLL.cs
+++ BLL/TidsregistreringBLL.cs
@@ -28,8 +28,66 @@ namespace BLL
 
         public static void OpretTidsregistrering(DateTime startTidspunkt, DateTime slutTidspunkt, int? sagsId, int medarbejderId)
         {
+            if (startTidspunkt >= slutTidspunkt)
+            {
+                throw new ArgumentException("Starttidspunkt skal ligge før sluttidspunkt");
+            }
+
+            CheckForOverlapPaaTidsregistrering(startTidspunkt, slutTidspunkt, medarbejderId);
+
+            CheckAntalTimerPaaUge(startTidspunkt, slutTidspunkt, medarbejderId);
+            
+            
+
+
+
+            
             TidsregistreringRepo.OpretTidsregistrering(startTidspunkt, slutTidspunkt, sagsId, medarbejderId);
+        }
+
+        private static void CheckAntalTimerPaaUge(DateTime startTidspunkt, DateTime slutTidspunkt, int medarbejderId)
+        {
+            List<DAL.model.Tidsregistrering> tidsregistreringerPaaUge = TidsregistreringRepo.GetTidsregistreringerPaaUge(startTidspunkt, medarbejderId);
+
+            TimeSpan samletTid = TimeSpan.Zero;
+
+            foreach (var t in tidsregistreringerPaaUge)
+            {
+                samletTid += t.SlutTidspunkt - t.StartTidspunkt;
+            }
+
+            TimeSpan nyVarighed = slutTidspunkt - startTidspunkt;
+            samletTid += nyVarighed;
+
+            int grænseTimer = 37;
+            if (samletTid.TotalHours > grænseTimer)
+            {
+                TimeSpan overskud = samletTid - TimeSpan.FromHours(grænseTimer);
+                string overskudTekst = $"{(int)overskud.TotalHours} timer og {overskud.Minutes} minutter";
+
+                throw new InvalidOperationException(
+                    $"Tidsregistreringen vil bringe medarbejderens ugearbejdstid op på {samletTid.TotalHours:F1} timer, hvilket overskrider grænsen på {grænseTimer} timer med {overskudTekst}.");
+            }
+        }
+
+
+
+        private static void CheckForOverlapPaaTidsregistrering(DateTime startTidspunkt, DateTime slutTidspunkt, int medarbejderId)
+        {
+            List<DAL.model.Tidsregistrering> tidsregistreringerPaaDato = TidsregistreringRepo.GetTidsregistreringerPaaDato(startTidspunkt, medarbejderId);
+
+
+            foreach (var eksisterendePaaDato in tidsregistreringerPaaDato)
+            {
+                bool overlapper = startTidspunkt < eksisterendePaaDato.SlutTidspunkt && slutTidspunkt > eksisterendePaaDato.StartTidspunkt;
+                if (overlapper)
+                {
+                    string fejlbesked = $"Tidsregistreringen overlapper med en eksisterende registrering fra {eksisterendePaaDato.StartTidspunkt:HH:mm} til {eksisterendePaaDato.SlutTidspunkt:HH:mm}.";
+                    throw new ArgumentException(fejlbesked);
+                }
+            }
 
         }
+
     }
 }
